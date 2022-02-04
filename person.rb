11c58b14775b52ac1bb24e872f995c6d99c3a12a
Jim Ntare <jntare@Jims-BK-MacBook-Pro.local>
2022-02-04 10:14:47 +0200

    create corrector class
    add it to the person class
    create validate method to capitalize name

--- person.rb
+++ person.rb
@@ -1,3 +1,5 @@
+require './corrector'
+
 class Person
   attr_accessor :name, :age
   attr_reader :id
@@ -7,10 +9,15 @@ class Person
     @name = name
     @parent_permission = parent_permission
     @age = age
+    @capitalize_name = Corrector.new
   end
 
   def can_use_services?
     of_age? || @parent_permission
+  end
+
+  def validate_name
+    @name = capitalize_name.correct_name(@name)
   end
 
   private
